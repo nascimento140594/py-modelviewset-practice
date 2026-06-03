692ef58c65e4b0edcc3b15f7822613095cb8bada
Matheus Araujo <nascimento140594@gmail.com>
2026-06-03 10:08:49 -0300

    Fix flake8 formatting in views

--- author/views.py
+++ author/views.py
@@ -1,9 +1,9 @@
-from rest_framework import viewsets
+from rest_framework.viewsets import ModelViewSet
+
 from .models import Author
 from .serializers import AuthorSerializer
 
 
-class AuthorViewSet(viewsets.ModelViewSet):
+class AuthorViewSet(ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
-    
